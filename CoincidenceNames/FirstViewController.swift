1240fe342c6982b6f2a96d08c63b38a7c2627d04
ElenaWorlt <da5hl@ya.ru>
2022-12-19 20:23:07 +0500

    create first part of app

---
+++ CoincidenceNames/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CoincidenceNames
+//
+//  Created by Елена Душакова on 19.12.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var nameLabelTF: UITextField!
+    @IBOutlet weak var partnerLabelTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func resultButton() {
+    }
+    
+}
+
